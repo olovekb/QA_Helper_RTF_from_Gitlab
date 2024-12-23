ec25ba6ab2e1be88c7a42510aa3d6232fa9b219e
vpoliakov <vpoliakov@nopaper.ru>
2024-12-23 11:39:16 +0500

    fix bug

--- server/http-service.mjs
+++ server/http-service.mjs
@@ -25,7 +25,7 @@ export async function getJwtToken() {
             {
                 headers: {
                     "Accept": "application/json",
-                    "Expect": "",
+                   // "Expect": "",
                 }
             }
         );
