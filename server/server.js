eaea2cc97afdd914c66e20e008290ca43afbe380
vpoliakov <vpoliakov@nopaper.ru>
2025-03-26 13:07:30 +0500

    up limit express json

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = 5000;
 
 // Настройка CORS
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 
 // Ограничение на количество параллельных запросов
 const limit = pLimit(100);
