a3d6d8e125bf8febb45ad5b8950fc74f65069888
vpoliakov <vpoliakov@nopaper.ru>
2025-04-03 17:15:14 +0500

    up project

--- client/src/index.js
+++ client/src/index.js
@@ -19,7 +19,8 @@ const projects = [
   { id: 199, name: 'Test' },
   { id: 302, name: 'Nopaper 2.0' },
   { id: 133, name: 'Дизайн система' },
-  { id: 305, name: 'АФБ' }
+  { id: 305, name: 'АФБ' },
+  { id: 308, name: 'LKPFL' }
 ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
