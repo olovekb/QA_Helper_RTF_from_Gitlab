84d64b3b203d8d5eb95d4e4fcc69d070a46ddd98
vpoliakov <vpoliakov@nopaper.ru>
2025-08-25 17:50:51 +0500

    +

--- server/server.js
+++ server/server.js
@@ -1314,7 +1314,7 @@ function extractToolArgs(aiResponse, preferredFnName) {
 //
 async function callWithBackoff(url, promptOrMessages, apiKey, opts = {}) {
     const {
-        model = 'microsoft/mai-ds-r1:free',
+        model = 'deepseek/deepseek-r1-0528:free',
         tools,
         tool_choice,
         response_format,
